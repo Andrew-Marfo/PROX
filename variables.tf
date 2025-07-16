303c2da06b562031712e8f64a11fe980e7b5c5aa
Andrew Marfo <andrewmarfo23@gmail.com>
2025-07-16 04:05:22 +0000

    Add Redshift variables to Terraform configuration

--- variables.tf
+++ variables.tf
@@ -33,3 +33,39 @@ variable "db_tables" {
   description = "Comma-separated list of tables to extract from RDS"
   type        = string
 }
+
+variable "redshift_cluster_identifier" {
+  description = "Redshift cluster identifier"
+  type        = string
+}
+
+variable "redshift_node_type" {
+  description = "Redshift node type (e.g., ra3.xlplus, dc2.large)"
+  type        = string
+}
+
+variable "redshift_master_username" {
+  description = "Redshift master username"
+  type        = string
+}
+
+variable "redshift_master_password" {
+  description = "Redshift master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "redshift_cluster_type" {
+  description = "Redshift cluster type (single-node or multi-node)"
+  type        = string
+}
+
+variable "redshift_subnet_ids" {
+  description = "List of subnet IDs for Redshift subnet group"
+  type        = list(string)
+}
+
+variable "redshift_vpc_id" {
+  description = "VPC ID for Redshift security group"
+  type        = string
+}
